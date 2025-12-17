f825d2d34e1d10c840bdcdc022cc65b2831279cf
defender737 <er7734@gmail.com>
2025-12-17 22:32:44 +0900

    feat: 아이디어 카드 컴포넌트에 삭제 아이콘 추가 및 props 추가

--- src/app/issue/_component/IssueCard.tsx
+++ src/app/issue/_component/IssueCard.tsx
@@ -12,26 +12,48 @@ import {
   VoteButton,
 } from './IssueCard.style';
 
-export default function IssueCard() {
+interface IssueCardProps {
+  content?: string;
+  author?: string;
+  isSelected?: boolean;
+  isVotePhrase?: boolean;
+  agreeCount?: number;
+  disagreeCount?: number;
+}
+
+export default function IssueCard(props: IssueCardProps) {
   return (
     <Card>
       <Header>
-        <Content>강남역 지하철 광고 집행</Content>
+        <Content>{props.content}</Content>
         <Meta>
-          <AuthorPill>Anna</AuthorPill>
-          <IconButton aria-label="comment">
-            <img
-              src="/comment.svg"
-              alt="댓글"
-            />
-          </IconButton>
+          <AuthorPill>{props.author}</AuthorPill>
+          {props.isVotePhrase ? (
+            <IconButton aria-label="comment">
+              <img
+                src="/comment.svg"
+                alt="댓글"
+              />
+            </IconButton>
+          ) : (
+            <IconButton aria-label="delete">
+              <img
+                src="/trash.svg"
+                alt="삭제"
+              />
+            </IconButton>
+          )}
         </Meta>
       </Header>
-      <Divider />
-      <Footer>
-        <VoteButton variant="primary">찬성</VoteButton>
-        <VoteButton variant="secondary">반대</VoteButton>
-      </Footer>
+      {props.isVotePhrase && (
+        <div>
+          <Divider />
+          <Footer>
+            <VoteButton variant="primary">찬성 {props.agreeCount}</VoteButton>
+            <VoteButton variant="secondary">반대 {props.disagreeCount}</VoteButton>
+          </Footer>
+        </div>
+      )}
     </Card>
   );
 }
