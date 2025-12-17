5e8e8383401a44c783ca2291b7d8d79658de789f
hungerbk <wjantlr@gmail.com>
2025-12-17 23:49:21 +0900

    refactor: Sidebar 컴포넌트 props 전달받는 방식으로 변경
    - Sidebar는 children을 받는 방식으로 변경
    - 사용하는 곳에서 children 요소 입력
    - SidebarItemProps interface 적용(title, href, status)

--- src/app/issue/layout.tsx
+++ src/app/issue/layout.tsx
@@ -1,10 +1,22 @@
 import Sidebar from '@/common/Sidebar/Sidebar';
+import SidebarItem from '@/common/Sidebar/SidebarItem';
+import * as S from '@/common/Sidebar/styles';
 
 export default function layout() {
   return (
     <>
       <header>header</header>
-      <Sidebar />
+      <Sidebar>
+        <S.SidebarTitle>ISSUE MAP</S.SidebarTitle>
+        <S.SidebarTitle>ISSUE LIST</S.SidebarTitle>
+        <S.SidebarList>
+          <SidebarItem
+            title="new issue"
+            href="#"
+            status="open"
+          />
+        </S.SidebarList>
+      </Sidebar>
     </>
   );
 }
