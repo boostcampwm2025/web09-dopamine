9f385ccf42299ecbd2d385cbcfd8aa324ee85256
defender737 <er7734@gmail.com>
2026-01-12 22:40:51 +0900

    feat: redis 키 생성 로직 추가

---
+++ src/lib/redis-keys.ts
@@ -0,0 +1,13 @@
+export const redisKeys = {
+  issue: (issueId: string) => `issue:${issueId}`,
+  issueUser: (issueId: string, userId: string) => `issue:${issueId}:user:${userId}`,
+  issueIdeas: (issueId: string) => `issue:${issueId}:ideas`,
+  issueCategories: (issueId: string) => `issue:${issueId}:categories`,
+  idea: (ideaId: string) => `idea:${ideaId}`,
+  ideaComments: (ideaId: string) => `idea:${ideaId}:comments`,
+  category: (categoryId: string) => `category:${categoryId}`,
+  categoryIdeas: (categoryId: string) => `category:${categoryId}:ideas`,
+  voteCount: (issueId: string, ideaId: string) => `vote:${issueId}:${ideaId}`,
+  ideaVoteUsers: (ideaId: string) => `idea:${ideaId}:votes:users`,
+  comment: (commentId: string) => `comment:${commentId}`,
+};
