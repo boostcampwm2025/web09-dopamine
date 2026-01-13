d123eb0a0da87db6720f7cd71a41ee71c1c8700f
yongaricode <kang061313@gmail.com>
2026-01-13 19:08:18 +0900

    feat: 이슈 멤버 역할 타입 개선
    • MEMBER_ROLE 상수를 사용하여 멤버 역할 타입을 정의하도록 변경
    • 코드의 가독성과 유지보수성을 높이기 위해 타입을 상수로 관리함

--- src/types/issue.ts
+++ src/types/issue.ts
@@ -1,8 +1,8 @@
-import { ISSUE_STATUS } from '@/constants/issue';
+import { ISSUE_STATUS, MEMBER_ROLE } from '@/constants/issue';
 
 export type IssueStatus = (typeof ISSUE_STATUS)[keyof typeof ISSUE_STATUS];
 
-type MemberRole = 'OWNER' | 'MEMBER';
+type MemberRole = (typeof MEMBER_ROLE)[keyof typeof MEMBER_ROLE];
 
 export type IssueMember = {
   id: string;
