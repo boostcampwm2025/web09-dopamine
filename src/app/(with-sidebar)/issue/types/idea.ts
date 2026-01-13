30be3042b2d307868c0408c82f28ef2c9f679211
KimGeonWoo <samgkgkgk@naver.com>
2026-01-13 11:49:30 +0900

    feat:  'highlighted' 상태 추가

--- src/app/(with-sidebar)/issue/types/idea.ts
+++ src/app/(with-sidebar)/issue/types/idea.ts
@@ -28,4 +28,4 @@ export interface IdeaWithPosition {
   editable?: boolean;
 }
 
-export type CardStatus = 'needDiscussion' | 'selected' | 'default';
+export type CardStatus = 'needDiscussion' | 'selected' | 'default' | 'highlighted';
