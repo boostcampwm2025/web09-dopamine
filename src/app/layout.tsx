26f4c78ec9a109cc25997f015719aff7f42412c4
hungerbk <wjantlr@gmail.com>
2026-01-14 04:35:00 +0900

    feat: query-provider 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+import { QueryClient } from '@tanstack/react-query';
 import 'pretendard/dist/web/static/pretendard.css';
 import { Toaster } from 'react-hot-toast';
 import Modal from '@/components/modal/modal';
 import Tooltip from '@/components/tooltip/tooltip';
 import ThemeProvider from '@/providers/ThemeProvider';
+import { Providers } from '@/providers/query-provider';
 import EmotionRegistry from '@/styles/EmotionRegistry';
 import GlobalStyle from '@/styles/globalStyles';
 
@@ -15,13 +17,15 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <EmotionRegistry>
-          <ThemeProvider>
-            <GlobalStyle />
-            <Tooltip />
-            <Toaster />
-            <Modal />
-            {children}
-          </ThemeProvider>
+          <Providers>
+            <ThemeProvider>
+              <GlobalStyle />
+              <Tooltip />
+              <Toaster />
+              <Modal />
+              {children}
+            </ThemeProvider>
+          </Providers>
         </EmotionRegistry>
       </body>
     </html>
