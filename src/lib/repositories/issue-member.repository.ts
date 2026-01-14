1402ae45468a9901bdd3a767ae80e20dfbc3887f
hungerbk <wjantlr@gmail.com>
2026-01-14 18:33:01 +0900

    refactor: issue-member.repository에서 메서드 통합하여 export

--- src/lib/repositories/issue-member.repository.ts
+++ src/lib/repositories/issue-member.repository.ts
@@ -2,35 +2,37 @@ import { IssueRole } from '@prisma/client';
 import { PrismaTransaction } from '@/types/prisma';
 import { prisma } from '../prisma';
 
-export async function addIssueOwner(
-  tx: PrismaTransaction,
-  issueId: string,
-  userId: string,
-  role: IssueRole = IssueRole.OWNER,
-) {
-  return tx.issueMember.create({
-    data: {
-      issueId,
-      userId,
-      role,
-    },
-  });
-}
+export const issueMemberRepository = {
+  async addIssueOwner(
+    tx: PrismaTransaction,
+    issueId: string,
+    userId: string,
+    role: IssueRole = IssueRole.OWNER,
+  ) {
+    return tx.issueMember.create({
+      data: {
+        issueId,
+        userId,
+        role,
+      },
+    });
+  },
 
-export async function findMembersByIssueId(issueId: string) {
-  return prisma.issueMember.findMany({
-    where: {
-      issueId,
-      deletedAt: null,
-    },
-    select: {
-      role: true,
-      user: {
-        select: {
-          id: true,
-          displayName: true,
+  async findMembersByIssueId(issueId: string) {
+    return prisma.issueMember.findMany({
+      where: {
+        issueId,
+        deletedAt: null,
+      },
+      select: {
+        role: true,
+        user: {
+          select: {
+            id: true,
+            displayName: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  },
+};
