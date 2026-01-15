53b6b3479c4eea8e2bc48d5534f1c74cdd7f9a9a
seongwon seo <seongwon0903@gmail.com>
2026-01-15 16:36:18 +0900

    feat: 이슈 페이지에 SSE 연결 추가
    - useIssueEvents 훅을 통해 실시간 이벤트 수신
    - userId가 있을 때만 SSE 연결 활성화

--- src/app/(with-sidebar)/issue/[id]/page.tsx
+++ src/app/(with-sidebar)/issue/[id]/page.tsx
@@ -14,13 +14,14 @@ import { useFilterIdea } from '@/app/(with-sidebar)/issue/hooks/use-filter-idea'
 import { useIdeaStatus } from '@/app/(with-sidebar)/issue/hooks/use-idea-card';
 import { useIdeaOperations } from '@/app/(with-sidebar)/issue/hooks/use-idea-operations';
 import { useIssueData } from '@/app/(with-sidebar)/issue/hooks/use-issue-data';
+import { useIssueEvents } from '@/app/(with-sidebar)/issue/hooks/use-issue-events';
 import { useCanvasStore } from '@/app/(with-sidebar)/issue/store/use-canvas-store';
 import { useCategoryStore } from '@/app/(with-sidebar)/issue/store/use-category-store';
 import { useIdeaStore } from '@/app/(with-sidebar)/issue/store/use-idea-store';
 import LoadingOverlay from '@/components/loading-overlay/loading-overlay';
 import { useModalStore } from '@/components/modal/use-modal-store';
-import { getUserIdForIssue } from '@/lib/storage/issue-user-storage';
 import { ISSUE_STATUS } from '@/constants/issue';
+import { getUserIdForIssue } from '@/lib/storage/issue-user-storage';
 import IssueJoinModal from '../_components/issue-join-modal/issue-join-modal';
 import { useIssueStore } from '../store/use-issue-store';
 
@@ -63,6 +64,9 @@ const IssuePage = () => {
   // 1. 이슈 데이터 초기화
   const { isAIStructuring, isCreateIdeaActive, isVoteButtonVisible, isVoteDisabled } =
     useIssueData(issueId);
+
+  // SSE 연결
+  useIssueEvents({ issueId, enabled: !!userId });
 
   // 2. 아이디어 관련 작업
   const {
