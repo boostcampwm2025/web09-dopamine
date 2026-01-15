bea1a344e21b85f28d35084894ee8ba900ad88cb
hungerbk <wjantlr@gmail.com>
2026-01-15 14:00:59 +0900

    feat: 이슈 생성 시 isPending일 때 loading overlay 효과 추가

--- src/components/modal/issue-create-modal/issue-create-modal.tsx
+++ src/components/modal/issue-create-modal/issue-create-modal.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useQuickStartMutation } from '@/app/(with-sidebar)/issue/hooks/queries/use-issue-mutation';
+import LoadingOverlay from '@/components/loading-overlay/loading-overlay';
 import { generateRandomNickname } from '@/lib/utils/nickname';
 import { useModalStore } from '../use-modal-store';
 import * as S from './issue-create-modal.styles';
@@ -12,7 +13,7 @@ export default function CreateIssueModal() {
   const [ownerNickname, setOwnerNickname] = useState(generateRandomNickname());
   const { closeModal } = useModalStore();
 
-  const { mutate } = useQuickStartMutation(closeModal);
+  const { mutate, isPending } = useQuickStartMutation(closeModal);
 
   const handleQuickStart = async () => {
     if (!title.trim() || !ownerNickname.trim()) {
@@ -24,34 +25,37 @@ export default function CreateIssueModal() {
   };
 
   return (
-    <S.Container>
-      <S.InfoContainer>
-        <S.InputWrapper>
-          <S.InputTitle>이슈 제목</S.InputTitle>
-          <S.Input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="예시) 서비스 홍보 방안"
-          />
-        </S.InputWrapper>
-        <S.InputWrapper>
-          <S.InputTitle>표시될 닉네임</S.InputTitle>
-          <S.Input
-            value={ownerNickname}
-            onChange={(e) => setOwnerNickname(e.target.value)}
-            placeholder="예시) 생각하는 단무지"
-          />
-        </S.InputWrapper>
-      </S.InfoContainer>
+    <>
+      <S.Container>
+        <S.InfoContainer>
+          <S.InputWrapper>
+            <S.InputTitle>이슈 제목</S.InputTitle>
+            <S.Input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="예시) 서비스 홍보 방안"
+            />
+          </S.InputWrapper>
+          <S.InputWrapper>
+            <S.InputTitle>표시될 닉네임</S.InputTitle>
+            <S.Input
+              value={ownerNickname}
+              onChange={(e) => setOwnerNickname(e.target.value)}
+              placeholder="예시) 생각하는 단무지"
+            />
+          </S.InputWrapper>
+        </S.InfoContainer>
 
-      <S.Footer>
-        <S.SubmitButton
-          type="button"
-          onClick={handleQuickStart}
-        >
-          이슈 생성
-        </S.SubmitButton>
-      </S.Footer>
-    </S.Container>
+        <S.Footer>
+          <S.SubmitButton
+            type="button"
+            onClick={handleQuickStart}
+          >
+            이슈 생성
+          </S.SubmitButton>
+        </S.Footer>
+      </S.Container>
+      {isPending && <LoadingOverlay message="이슈를 생성하고 있습니다" />}
+    </>
   );
 }
