d393cff2790827b1fce885e33512d412c163866b
seongwon seo <seongwon0903@gmail.com>
2026-01-21 16:12:57 +0900

    refactor: 메인 페이지 서버/클라이언트 컴포넌트 분리 및 구조 개선
    - page.tsx를 서버 컴포넌트로 전환하여 세션 체크 최적화
    - UI 및 인터랙션 로직을 home-page로 분리
    - 로그인 유저 자동 리다이렉트 로직 제거

---
+++ src/components/home/home-page.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+import { Session } from 'next-auth';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import Background from '@/components/background/background';
+import CreateIssueModal from '@/components/modal/issue-create-modal/issue-create-modal';
+import { useModalStore } from '@/components/modal/use-modal-store';
+import * as S from './home-page.styles';
+
+const SOCIAL_ICONS = [
+  { src: '/github.svg', alt: 'github', provider: 'github' },
+  { src: '/google.svg', alt: 'google', provider: 'google' },
+  { src: '/kakao.svg', alt: 'kakao', provider: 'kakao' },
+  { src: '/naver.svg', alt: 'naver', provider: 'naver' },
+];
+
+interface HomePageProps {
+  session: Session | null;
+}
+
+export default function HomePage({ session }: HomePageProps) {
+  const router = useRouter();
+  const { openModal } = useModalStore();
+
+  const handleStart = () => {
+    if (session) {
+      router.push('/project');
+      return;
+    }
+
+    openModal({
+      title: '이슈 생성',
+      content: <CreateIssueModal />,
+      closeOnOverlayClick: true,
+      hasCloseButton: true,
+    });
+  };
+
+  const handleSocialLogin = (provider: string) => {
+    if (provider === 'google') {
+      signIn('google', { callbackUrl: '/project' });
+    } else {
+      // TODO: 다른 로그인 제공자 구현
+      alert('준비 중인 기능입니다.');
+    }
+  };
+
+  return (
+    <>
+      <Background />
+      <S.Container>
+        <S.LogoContainer>
+          <S.Logo>M</S.Logo>
+          <S.Text>Murphy</S.Text>
+        </S.LogoContainer>
+        <S.TitleContainer>
+          <S.Title>
+            발산은 <S.Highlight color="#3B82F6">자유롭게</S.Highlight>,
+          </S.Title>
+          <S.Title>
+            수렴은 <S.Highlight color="#00A94F">확실하게</S.Highlight>
+            <Image
+              src={'/check.svg'}
+              alt="check"
+              width={35}
+              height={35}
+              style={{ position: 'relative', top: -50 }}
+            />
+          </S.Title>
+        </S.TitleContainer>
+        <S.SubTitleContainer>
+          <S.Text>
+            아이디어 브레인스토밍부터 의사결정까지,{' '}
+            <S.Highlight
+              color="#000000"
+              background="#F0FDF4"
+            >
+              Murphy
+            </S.Highlight>{' '}
+            가 가장 스마트한
+          </S.Text>
+          <S.Text>길을 안내합니다.</S.Text>
+        </S.SubTitleContainer>
+        <S.BtnContainer>
+          <S.StartButton onClick={handleStart}>빠르게 시작하기</S.StartButton>
+        </S.BtnContainer>
+        <S.HorizontalLine />
+        <S.SocialLoginContainer>
+          {SOCIAL_ICONS.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={50}
+              height={50}
+              style={{ cursor: 'pointer' }}
+              onClick={() => handleSocialLogin(icon.provider)}
+            />
+          ))}
+        </S.SocialLoginContainer>
+      </S.Container>
+    </>
+  );
+}
