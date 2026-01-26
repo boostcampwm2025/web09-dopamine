4b88380da5499ed1a14aef0164e7b21c4a34ffff
hungerbk <wjantlr@gmail.com>
2026-01-27 06:05:39 +0900

    refactor: 불필요한 코드 삭제

--- src/app/(with-sidebar)/issue/_components/canvas/canvas-context.tsx
+++ src/app/(with-sidebar)/issue/_components/canvas/canvas-context.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { RefObject, createContext, useContext } from 'react';
+import { createContext, useContext } from 'react';
 
 interface CanvasContextValue {
   scale: number;
