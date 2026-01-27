075c3823ec7ef43b41f690ce83a84b93f8f9a74e
hungerbk <wjantlr@gmail.com>
2026-01-27 22:17:36 +0900

    feat: 토픽 상세에 뒤로가기 버튼 추가
    - 히스토리에 있는 뒤로가기로 이동

--- src/app/(with-sidebar)/topic/_components/header/topic-header.tsx
+++ src/app/(with-sidebar)/topic/_components/header/topic-header.tsx
@@ -1,25 +1,28 @@
 'use client';
 
 import Image from 'next/image';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { useTopicDetailQuery } from '@/hooks/topic';
 import CreateIssueButton from '../create-issue-button/create-issue-button';
 import * as S from './topic-header.styles';
 
 export default function TopicHeader() {
   const params = useParams();
+  const router = useRouter();
   const topicId = params.id as string;
   const { data: topic } = useTopicDetailQuery(topicId);
 
   return (
     <S.HeaderContainer>
       <S.LeftSection>
-        <Image
-          src="/leftArrow.svg"
-          alt="뒤로가기"
-          width={18}
-          height={18}
-        />
+        <button onClick={router.back}>
+          <Image
+            src="/leftArrow.svg"
+            alt="뒤로가기"
+            width={18}
+            height={18}
+          />
+        </button>
         {topic?.title}
       </S.LeftSection>
       <CreateIssueButton />
