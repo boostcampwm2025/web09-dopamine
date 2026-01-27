226df2e7e2f355fcfb1ad8085a7fe952f9db1f07
seongwon seo <seongwon0903@gmail.com>
2026-01-27 14:59:39 +0900

    refactor: UI에서 카카오 아이콘 제거

--- src/components/social-login/social-login.tsx
+++ src/components/social-login/social-login.tsx
@@ -10,18 +10,12 @@ interface SocialLoginProps {
 const SOCIAL_ICONS = [
   { src: '/github.svg', alt: 'github', provider: 'github' },
   { src: '/google.svg', alt: 'google', provider: 'google' },
-  { src: '/kakao.svg', alt: 'kakao', provider: 'kakao' },
   { src: '/naver.svg', alt: 'naver', provider: 'naver' },
 ];
 
 export default function SocialLogin({ callbackUrl = '/project', iconSize = 50 }: SocialLoginProps) {
   const handleSocialLogin = (provider: string) => {
-    if (
-      provider === 'google' ||
-      provider === 'github' ||
-      provider === 'kakao' ||
-      provider === 'naver'
-    ) {
+    if (provider === 'google' || provider === 'github' || provider === 'naver') {
       signIn(provider, { callbackUrl });
     }
   };
