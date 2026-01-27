fb80d2ab95ecc4f3008591abda257ed4f4e39cae
hungerbk <wjantlr@gmail.com>
2026-01-27 22:41:21 +0900

    refactor: 토픽 상세 뒤로가기 경로를 프로젝트 상세로 변경
    - router.back을 사용했을 때 이슈-토픽 무한 반복되는 문제 해결

--- src/app/(with-sidebar)/topic/_components/header/topic-header.tsx
+++ src/app/(with-sidebar)/topic/_components/header/topic-header.tsx
@@ -1,28 +1,28 @@
 'use client';
 
 import Image from 'next/image';
-import { useParams, useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
 import { useTopicDetailQuery } from '@/hooks/topic';
 import CreateIssueButton from '../create-issue-button/create-issue-button';
 import * as S from './topic-header.styles';
 
 export default function TopicHeader() {
   const params = useParams();
-  const router = useRouter();
   const topicId = params.id as string;
   const { data: topic } = useTopicDetailQuery(topicId);
 
   return (
     <S.HeaderContainer>
       <S.LeftSection>
-        <button onClick={router.back}>
+        <Link href={`/project/${topic?.projectId}`}>
           <Image
             src="/leftArrow.svg"
             alt="뒤로가기"
             width={18}
             height={18}
           />
-        </button>
+        </Link>
         {topic?.title}
       </S.LeftSection>
       <CreateIssueButton />
