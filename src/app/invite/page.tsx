b65aa29762e45768f159eecf10ca1053eb5441ee
yongaricode <kang061313@gmail.com>
2026-02-02 20:04:22 +0900

    feat: 초대 페이지 제목에 사이트명 추가

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -3,7 +3,7 @@ import { InvitationService } from '@/lib/services/invitation.service';
 import { InvitationContainer } from './invitation-container';
 
 export const metadata: Metadata = {
-  title: '프로젝트 초대 링크',
+  title: 'Murphy | 프로젝트 초대 링크',
   description: 'Murphy 프로젝트 초대 링크입니다. 링크를 통해 참여해보세요',
 };
 
