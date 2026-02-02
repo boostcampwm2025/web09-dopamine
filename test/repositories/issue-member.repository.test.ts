7d59db0f02a9bc2fa3fd982910cd127f6cfd81fc
seongwon seo <seongwon0903@gmail.com>
2026-02-02 11:57:22 +0900

    test: findMemberByNickname 테스트 제거

--- test/repositories/issue-member.repository.test.ts
+++ test/repositories/issue-member.repository.test.ts
@@ -59,30 +59,6 @@ describe('Issue Member Repository 테스트', () => {
         userId: true,
         role: true,
         nickname: true,
-        user: {
-          select: {
-            displayName: true,
-            name: true,
-          },
-        },
-      },
-    });
-  });
-
-  it('닉네임으로 이슈 멤버 존재 여부를 확인한다', async () => {
-    // 역할: 닉네임 중복 체크가 정확해야 초대/가입 로직이 안정적으로 동작한다.
-    mockedIssueMember.findFirst.mockResolvedValue({ id: 'member-1' } as any);
-
-    await issueMemberRepository.findMemberByNickname('issue-1', '닉네임');
-
-    expect(mockedIssueMember.findFirst).toHaveBeenCalledWith({
-      where: {
-        issueId: 'issue-1',
-        deletedAt: null,
-        nickname: '닉네임',
-      },
-      select: {
-        id: true,
       },
     });
   });
@@ -103,12 +79,6 @@ describe('Issue Member Repository 테스트', () => {
         userId: true,
         nickname: true,
         role: true,
-        user: {
-          select: {
-            displayName: true,
-            name: true,
-          },
-        },
       },
     });
   });
