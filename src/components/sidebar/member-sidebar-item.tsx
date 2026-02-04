1c105b6257f4cf6f0d89ca2c810f5088ed6cee03
KimGeonWoo <samgkgkgk@naver.com>
2026-02-04 15:39:49 +0900

    feat: 닉네임 편집 UI 수정 - 아이콘 색상 변경 - 이슈 페이지일 때만 표시되도록 수정

--- src/components/sidebar/member-sidebar-item.tsx
+++ src/components/sidebar/member-sidebar-item.tsx
@@ -109,7 +109,7 @@ export default function MemberSidebarItem({
             </MemberS.OwnerBadge>
           )}
 
-          {isCurrentUser && (
+          {(isCurrentUser && isIssuePage) && (
             <MemberS.ActionContainer>
               {isEditing ? (
                 <>
@@ -124,7 +124,7 @@ export default function MemberSidebarItem({
               ) : (
                 <MemberS.IconButton onClick={startEditing} title="닉네임 수정">
                   <Image
-                    src="/edit.svg"
+                    src="/edit-gray.svg"
                     alt="닉네임 수정"
                     width={14}
                     height={14}
